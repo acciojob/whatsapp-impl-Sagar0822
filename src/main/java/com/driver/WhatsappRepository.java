5c176ce73ca9ab67191580770c69842143eefb3a
Sagar0822 <srsarode214@gmail.com>
2023-03-22 22:58:43 +0530

    solution 1

--- src/main/java/com/driver/WhatsappRepository.java
+++ src/main/java/com/driver/WhatsappRepository.java
@@ -14,6 +14,7 @@ public class WhatsappRepository {
     private HashMap<Message, User> senderMap;
     private HashMap<Group, User> adminMap;
     private HashSet<String> userMobile;
+
     private int customGroupCount;
     private int messageId;
 
@@ -25,5 +26,116 @@ public class WhatsappRepository {
         this.userMobile = new HashSet<>();
         this.customGroupCount = 0;
         this.messageId = 0;
+    }
+
+    public String createUser(String name, String mobile) throws Exception{
+        if(userMobile.contains(mobile)){
+            throw new Exception("User already exists");
+        }
+        User user = new User(name,mobile);
+        userMobile.add(mobile);
+
+        return "SUCCESS";
+    }
+
+    public Group createGroup(List<User> users){
+        String groupName = "";
+       if(users.size() == 2){    //for personal chat
+          groupName = users.get(1).getName();
+       }
+       else{
+           customGroupCount++;
+           groupName = "Group " + customGroupCount;
+       }
+       Group group = new Group(groupName , users.size());
+       groupUserMap.put(group , users);
+       adminMap.put(group , users.get(0));
+       groupMessageMap.put(group , new ArrayList<>());
+
+       return group;
+    }
+
+    public int createMessage(String content){
+        messageId++;
+        Message message  = new Message(messageId , content);
+
+        return messageId;
+    }
+
+    public int sendMessage(Message message, User sender, Group group) throws Exception{
+
+        if(!groupUserMap.containsKey(group)){
+            throw new Exception("Group does not exist");
+        }
+
+        if(!groupUserMap.get(group).contains(sender)){
+            throw new Exception("You are not allowed to send message");
+        }
+
+        List<Message> list  = groupMessageMap.get(group);
+        list.add(message);
+        groupMessageMap.put(group , list);
+        senderMap.put(message , sender);
+
+        return list.size();
+    }
+
+    public String changeAdmin(User approver, User user, Group group) throws Exception{
+        if(!groupUserMap.containsKey(group)){
+            throw new Exception("Group does not exist");
+        }
+
+        if(!adminMap.get(group).equals(approver)){
+            throw new Exception("Approver does not have rights");
+        }
+
+        if(!groupUserMap.get(group).contains(user)){
+            throw new Exception("User is not a participant");
+        }
+        adminMap.put(group, user);
+
+        return "SUCCESS";
+    }
+
+    public int removeUser(User user) throws Exception{
+        boolean isPresent = false;
+        Group gp = null;
+
+        for(User u : adminMap.values()){
+            if(u.equals(user))
+                throw new Exception("Cannot remove admin");
+        }
+
+        for(Group group : groupUserMap.keySet()){
+            if(groupUserMap.get(group).contains(user)){
+                groupUserMap.get(group).remove(user);
+                userMobile.remove(user);
+                isPresent = true;
+                gp = group;
+            }
+        }
+        if(!isPresent)  throw new Exception("User not found");
+
+        for(Message m: senderMap.keySet()){
+            if(senderMap.get(m).equals(user)){
+                senderMap.remove(m);
+                groupMessageMap.get(gp).remove(m);
+            }
+        }
+
+        return groupUserMap.get(gp).size() + groupMessageMap.get(gp).size() + senderMap.size();
+    }
+
+    public String findMessage(Date start, Date end, int k) throws Exception{
+        Map<Integer,String> map = new TreeMap();
+        for(Message m: senderMap.keySet()){
+            if(start.compareTo(m.getTimestamp())<0 && m.getTimestamp().compareTo(end)<0){
+                map.put(m.getId(),m.getContent());
+            }
+        }
+        if(map.size()<k) throw new Exception("K is greater than the number of messages");
+
+        List<String> list = new ArrayList<>(map.values());
+        return list.get(list.size()-k);
     }
 }
